b0c100257d2e0776f8af5bb58eb60641590f5ad5
Sriram <rsriram.10689@gmail.com>
2014-08-14 13:02:32 +0530

    initial commit

---
+++ src/firstclass.java
@@ -0,0 +1,13 @@
+
+public class firstclass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My first Java project");
+
+	}
+
+}
